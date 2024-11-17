afd9cacd2a3d479d66000166e5d0d3ba9fb2b3b1
Chanathip <chanathip.cha@outlook.com>
2024-11-17 23:51:53 +0700

    Sent

--- test_zoo.py
+++ test_zoo.py
@@ -4,10 +4,24 @@ from zoo import Zoo
 class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
+    
+    def testErr(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1),"err")
+    
+    def testNobirth(self):
+        self.assertEqual(self.zoo.get_ticket_price(0),50)
 
-    def test_child_ticket_price(self):
+    def testChild(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
+    def testTeen(self):
+        self.assertEqual(self.zoo.get_ticket_price(13),100)
+    
+    def testAdultTicket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21),150)
+
+    def testOldPrice(self):
+        self.assertEqual(self.zoo.get_ticket_price(70),100)
     # Add your additional test cases here.
 
 if __name__ == '__main__':
